be2d660320e45977933fd393b05bda717e8e025d
lilacilla <149900581+lilacilla@users.noreply.github.com>
2024-06-11 19:47:42 +0100

    Update FormattedDate.js

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -22,5 +22,9 @@ export default function FormattedDate(props) {
     minutes = `0${minutes}`;
   }
 
-  return `${day} ${hours}:${minutes}`;
+  return (
+    <span>
+      {day} {hours}:{minutes}
+    </span>
+  );
 }
